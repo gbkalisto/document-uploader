cacf7b1cef411bba6a094ff1979282b6ecad8305
shubham sharma <shubhamskv5@gmail.com>
2026-03-24 17:34:20 +0530

    modified app provider again

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,8 +18,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            UserSeeder::class,
-            // AdminSeeder::class
+            // UserSeeder::class,
+            AdminSeeder::class
         ]);
     }
 }
