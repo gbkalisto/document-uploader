9e265e7336464da97e4c17bd2b2fdf0d5a6f6211
shubham sharma <shubhamskv5@gmail.com>
2026-03-24 17:31:02 +0530

    modified app provider again1

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -33,10 +33,15 @@ class AppServiceProvider extends ServiceProvider
 
         // Otherwise (Local), use the default 'build' directory
         if (file_exists(base_path('../public_html'))) {
+            // 1. Internal Disk Path (Keep this as is so Laravel can find the file)
             Vite::useBuildDirectory('../../public_html/build');
 
-            // Force the asset URL to point to your domain root
-            Vite::useHotFile(base_path('../public_html/hot'));
+            // 2. Browser URL Path (Change this so the link in the source code looks correct)
+            Vite::withEntryPoints(['resources/css/app.css', 'resources/js/app.js'])
+                ->useManifestFilename('../../public_html/build/manifest.json');
+
+            // If the above doesn't work, use this simpler override:
+            Vite::useBuildDirectory('build');
         }
     }
 }
